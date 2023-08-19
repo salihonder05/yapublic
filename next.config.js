0251fc05b6443a5b939d6ea92156a9c683baf47b
Mehmet Faruk Andaç <83770786+mehmetfarukandac@users.noreply.github.com>
2023-08-19 19:02:52 +0300

    url değiştirildi 19:02:55
    url değiştirildi 19:02:55

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
     env: {
         API_URL: 'http://api-v3.fastrest.com.tr:4002/graphql/',
-        PROJECT_API_URL: 'http://localhost:3000/api/',
+        PROJECT_API_URL: 'https://yapublic.vercel.app/api/ ',
+        // PROJECT_API_URL: 'http://localhost:3000/api/',
     },
     images: {
         domains: ['admin.fastrest.com.tr', 'dummyimage.com', 'cdn.fastrest.com.tr'],
