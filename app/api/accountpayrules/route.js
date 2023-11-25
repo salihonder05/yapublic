c877714b4b8560df313cc2c32d80341c0c1b9dd6
Mehmet Faruk Andac <farukandac@bilfen.com>
2023-11-25 15:16:04 +0300

    last commti
    last commit

--- app/api/accountpayrules/route.js
+++ app/api/accountpayrules/route.js
@@ -1,9 +1,8 @@
 export async function POST(req) {
-    const { accountId, payruleTypeId } = await req.json();
+  const { accountId, payruleTypeId } = await req.json();
 
-
-    const API_URL = process.env.API_URL;
-    const query = `
+  const API_URL = process.env.API_URL;
+  const query = `
             query { 
                 account_payrules(payrule_account: ${accountId}, payrule_type: ${payruleTypeId}) {
                     id
@@ -14,25 +13,27 @@ export async function POST(req) {
                 }  
             }`;
 
-    try {
-        const response = await fetch(API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ query }),
-        });
-        const data = await response.json();
-        if (response.ok) {
-            return new Response(JSON.stringify({ data }), {
-                headers: { "Content-Type": "application/json", "Cache-Control": "no-store" },
-                status: 200,
-            });
-
-        } else {
-            console.error("Form submission failed");
-        }
-    } catch (error) {
-        console.error("Error submitting form:", error);
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    });
+    const data = await response.json();
+    if (response.ok) {
+      return new Response(JSON.stringify({ data }), {
+        headers: {
+          "Content-Type": "application/json",
+          "Cache-Control": "no-store",
+        },
+        status: 200,
+      });
+    } else {
+      console.error("Form submission failed");
     }
-};
+  } catch (error) {
+    console.error("Error submitting form:", error);
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        API_URL: 'http://api-v3.fastrest.com.tr:4002/graphql/',
-        // PROJECT_API_URL: 'https://yapublic.vercel.app/api/ ',
-        PROJECT_API_URL: 'http://localhost:3000/api/',
-    },
-    images: {
-        domains: ['admin.fastrest.com.tr', 'dummyimage.com', 'cdn.fastrest.com.tr'],
-    },
+  env: {
+    API_URL: "http://api-v3.fastrest.com.tr:4002/graphql/",
+    // PROJECT_API_URL: 'https://yapublic.vercel.app/api/ ',
+    PROJECT_API_URL: "http://localhost:3000/api/",
+  },
+  images: {
+    domains: ["admin.fastrest.com.tr", "dummyimage.com", "cdn.fastrest.com.tr"],
+  },
 };
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
